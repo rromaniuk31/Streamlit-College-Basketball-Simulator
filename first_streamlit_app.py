3c9109e52036900f00bc939db0211abed6354bba
Raymond Romaniuk <84481771+rromaniuk31@users.noreply.github.com>
2026-03-21 17:47:26 -0400

    Update app with team logos

--- first_streamlit_app.py
+++ first_streamlit_app.py
@@ -4,10 +4,31 @@ import streamlit as st
 from collections import Counter
 
 # Local
-#df = pd.read_csv("/Users/acer1/Downloads/team_avgs2026.csv", header = None)
-df = pd.read_csv("team_avgs2026.csv", header = None)
+df = pd.read_csv("/Users/acer1/Downloads/team_avgs2026.csv", header = None)
+#df = pd.read_csv("team_avgs2026.csv", header = None)
 df = df.loc[1:]
 df = df.rename(columns = {0: "team", 1: "PPG", 2: "PAPG"})
+winner = ""
+
+import base64
+
+def display_logo(team, is_winner):
+    st.empty()
+    border_style = "5px solid green" if is_winner else "none"
+    
+    with open(f"logos/{team}.png", "rb") as f:
+        img_bytes = f.read()
+        encoded = base64.b64encode(img_bytes).decode()
+    
+    st.markdown(
+        f"""
+        <div style="display: flex; justify-content: center;">
+            <img src="data:image/png;base64,{encoded}"
+                 style="border: {border_style}; border-radius: 10px; padding: 1px; width:300px;">
+        </div>
+        """,
+        unsafe_allow_html=True
+    )
 
 
 def sim_game(team1, team2, stats_df, n_sim):
@@ -58,6 +79,21 @@ second_team_choice = st.selectbox(
 if second_team_choice:
     second_team_df = df[df["team"] == second_team_choice]
     team2 = second_team_choice
+
+    col1, col2, col3 = st.columns(3)
+    with col1:
+        team1_placeholder = st.empty()
+        #display_logo(team1, team1 == winner)
+    with col2:
+        #st.write("vs")
+        st.markdown("<h3 style='text-align: center;'>vs</h3>", unsafe_allow_html = True, text_alignment = "center")
+    with col3:
+        team2_placeholder = st.empty()
+        #display_logo(team1, team2 == winner)
+
+    team1_placeholder.image(f"logos/{team1}.png")
+    team2_placeholder.image(f"logos/{team2}.png")
+
 else:
     st.write("Please select a team to simulate.")
 
@@ -72,8 +108,21 @@ if st.button("Run Simulation"):
     print(winner_array)
 
     probs = {team1: round(sum(winner_array) / len(winner_array), 2), team2: round(1 - (sum(winner_array) / len(winner_array)), 2)}
-    #total = sum(results.values())
-    #probs = {k: v / total for k, v in results.items()}
+    winner = max(probs, key = probs.get)
+    #with col1:
+    #    team1_placeholder.markdown(
+    #        display_logo(team1, team1 == winner),
+    #        unsafe_allow_html=True
+    #    )
+    #with col3:
+    #    team2_placeholder.markdown(
+    #        display_logo(team2, team2 == winner),
+    #        unsafe_allow_html=True
+    #    )
+    with team1_placeholder:
+        display_logo(team1, team1 == winner)
+    with team2_placeholder:
+        display_logo(team2, team2 == winner)
     
     st.write("Win Probabilities")
     st.dataframe(probs)
